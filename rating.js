43b2f0964d3693e47b4a6aad6015372f1e1b6aa5
Zachary Martin <zmartin8@uncc.edu>
2020-03-31 16:07:44 -0400

    oh my gosh this code is bad. The stars highlight though

---
+++ rating.js
@@ -0,0 +1,49 @@
+function getSelectedText(){
+	var $text = null;
+	if(window.getSelection()!=null){
+		$text=window.getSelection().toString();
+	}
+	
+	return $text;
+}
+
+function rateText(s){
+	$text = s
+	
+	$text.css('color':'yellow');
+	s=$text;
+	return s;
+	
+}
+
+function submitRating(string){
+	var stars = 0;
+	for(i=0;i<string.length;i++){
+		if(string[i]==="&#9734;"){
+			stars++;
+		}
+	}
+	return stars;
+	
+}
+
+
+
+
+$(window).load(function() {
+    var $text =getSelectedText();
+	if($text!=null){
+		$text = rateText($text);
+	}
+	
+	$("#plz").click(function () {
+   alert("lol");
+});
+	
+	
+	
+	
+	
+	
+        
+        });
