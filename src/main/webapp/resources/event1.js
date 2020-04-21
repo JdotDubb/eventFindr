53e04f59cf78106cf9e4f12f4fea743849efaa46
josh.weatherford <josh.weatherford@44L93C6.corp.southwoodrealty.com>
2020-04-21 15:40:04 -0400

    Adding API calls for eventful and edit Discover page.

--- src/main/webapp/resources/event1.js
+++ src/main/webapp/resources/event1.js
@@ -1,17 +1,19 @@
-(function () {
-    var flickerAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    $.getJSON(flickerAPI, {
-        tags: "music",
-        tagmode: "any",
-        format: "json"
-    })
-            .done(function (data) {
-                $.each(data.items, function (i, item) {
-                    $("<img>").attr("src", item.media.m).appendTo("#images");
-                    if (i === 0) {
-                        return false;
-                    }
-                });
-            });
-})();
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
 
